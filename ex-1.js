02c62bd6b173dfccba73a610256cfee76a9eab80
sarai.arisa <sarai.arisa@bitkub.com>
2023-08-22 20:31:55 +0700

    correct function

--- ex-1.js
+++ ex-1.js
@@ -1,4 +1,17 @@
 // Exercise #1 Declaring Functions
-// Start coding here\
-
-let test 
+// Start coding here
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
